e639d7a7cf8cee4d49ea15db96d84e9a874200ed
Evgeny Lim <evgeny.lim4@gmail.com>
2025-01-25 14:39:41 +0300

    3. Массивы и модели. [#174892 #337705]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -7,21 +7,17 @@ public class Library {
         Book learnHowToCode = new Book("Learn how to code", 1787);
         Book elonMusk = new Book("Elon Musk", 2045);
         Book[] books = new Book[] {cleanCode, headFirstJava, learnHowToCode, elonMusk};
-
         for (Book book : books) {
             System.out.println(book.getName() + " - " + book.getPageCount() + " pages");
         }
-
         System.out.println();
         Book temp = books[0];
         books[0] = books[3];
         books[3] = temp;
-
         System.out.println("Replace first book with the last one");
         for (Book book : books) {
             System.out.println(book.getName() + " - " + book.getPageCount() + " pages");
         }
-
         System.out.println();
         System.out.println("Looking for a book with name \"Clean Code\"");
         for (Book book : books) {
