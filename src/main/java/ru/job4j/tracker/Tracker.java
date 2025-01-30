0b0ac806f4cd4743e2af5fae7f50bdedfa02a63c
Evgeny Lim <evgeny.lim4@gmail.com>
2025-01-30 07:31:36 +0300

    5. Tracker - хранилище [#396 #338022]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,23 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int length = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (key.equals(item.getName())) {
+                result[length] = item;
+                length++;
+            }
+        }
+        return Arrays.copyOf(result, length);
     }
 
     public Item findById(int id) {
