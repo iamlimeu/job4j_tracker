75a4af3adc9dcce82eb4165943cb894dd922e6f3
Evgeny Lim <evgeny.lim4@gmail.com>
2025-01-30 08:54:41 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #338364]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -55,4 +55,14 @@ public class Tracker {
         }
         return false;
     }
+
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            int length = size - index - 1;
+            System.arraycopy(items, index + 1, items, index, length);
+            items[size - 1] = null;
+            size--;
+        }
+    }
 }
