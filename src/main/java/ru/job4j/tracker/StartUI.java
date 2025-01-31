ffd5ea777c92882ece128d2d8097e71879119baf
Evgeny Lim <evgeny.lim4@gmail.com>
2025-01-31 09:30:21 +0300

    10. toString [#310018 #338404]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,7 +4,7 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item dateAndTime = new Item();
-        System.out.println(dateAndTime.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
+        Item item = new Item("Eugene", 1);
+        System.out.println(item);
     }
 }
