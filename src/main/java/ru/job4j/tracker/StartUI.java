732efceeac45727d84a7ae2068803779a99cb6c0
Evgeny Lim <evgeny.lim4@gmail.com>
2025-02-07 20:09:19 +0300

    3.3. Реализация класса StartUI. Изменение заявки [#500746 #339233]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -27,6 +27,18 @@ public class StartUI {
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
                 }
+            } else if (select == 2) {
+                System.out.println("=== Редактирование заявки ===");
+                System.out.print("Введите номер id заявки: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.print("Введите желаемое имя заявки: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                if (tracker.replace(id, item)) {
+                    System.out.println("Заявка успешна изменена: " + item);
+                } else {
+                    System.out.println("Ошибка замены заявки");
+                }
             } else if (select == 6) {
                 run = false;
             }
