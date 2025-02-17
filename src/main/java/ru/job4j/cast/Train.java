d3045a297b92d2fa378eb30f24a40239ee0c8c03
Evgeny Lim <evgeny.lim4@gmail.com>
2025-02-17 17:16:20 +0300

    5.1. Зачем нужно приведение типов [#310019 #341572]

---
+++ src/main/java/ru/job4j/cast/Train.java
@@ -0,0 +1,9 @@
+package ru.job4j.cast;
+
+public class Train implements Vehicle {
+
+    @Override
+    public void move() {
+        System.out.println(getClass().getSimpleName() + " is moving on the rails");
+    }
+}
