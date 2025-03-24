01a229024dd9a642d02a70d7f6b42cc4fd2bec18
Evgeny Lim <evgeny.lim4@gmail.com>
2025-03-24 20:00:24 +0300

    1. Паспорт и жители [#212656 #537219]

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -8,8 +8,7 @@ public class PassportOffice {
 
     public boolean add(Citizen citizen) {
         boolean result = false;
-        String key = citizen.getPassport();
-        if (!citizens.containsKey(key)) {
+        if (!citizens.containsKey(citizen.getPassport())) {
             citizens.put(citizen.getPassport(), citizen);
             result = true;
         }
